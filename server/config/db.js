c7e4f0f96eaf8f410257767b1bec1b3c3d346643
Amanda Ike <amandayurike@gmail.com>
2023-03-01 17:32:05 -0500

    included connection to mongodb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://127.0.0.1/book_store"
+    //"URI": "mongodb://127.0.0.1/book_store"
+    "URI": "mongodb+srv://comp229-lab:xpftrQAJi3SCZNrA@mongodbserver.ghhuqpr.mongodb.net/book_store?retryWrites=true&w=majority"
 }
